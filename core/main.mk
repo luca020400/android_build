c51c912bb86267c367d41b81cddc10c7cfbb298f
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-09-02 10:49:43 +0200

    Add back qcom_utils.mk
    OPS

--- core/main.mk
+++ core/main.mk
@@ -144,6 +144,9 @@ endif
 # be generated correctly
 include $(BUILD_SYSTEM)/cleanbuild.mk
 
+# Bring in Qualcomm helper macros
+include $(BUILD_SYSTEM)/qcom_utils.mk
+
 # Include the google-specific config
 -include vendor/google/build/config.mk
 
